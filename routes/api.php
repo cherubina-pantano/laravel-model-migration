8196e0d14c272f89ebbaa454c19b74be7f17c6a9
cherubina-pantano <cherubina.pantano@gmail.com>
2021-01-20 16:10:34 +0100

    Create API (file json)

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+//In questo caso non funziona
+//Route::get('/bikes', 'BikeController@index');
+
+//Cerca il controller (per le API) in base al namespace se la path di defoult viene cambiata aggiungendo una nuova cartella 
+Route::namespace('Api')->group(function() {
+    Route::get('/bikes', 'BikeController@index');
 });
